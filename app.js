d30100f02be4e818002036914e9b795616704097
Bilal Shah <bshah_5@hotmail.co.uk>
2025-11-10 10:39:12 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require("express");
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.use("/api", express.static("public"));
+
 const { getTopics } = require("./controllers/topics.controller");
 const { getUsers } = require("./controllers/users.controller");
 const { getArticles, getArticleById, patchArticleVotes } = require("./controllers/articles.controller");
 const { getCommentsForArticle, addCommentToArticle, deleteCommentById } = require("./controllers/comments.controller");
 
 const { handleNotFoundError, handleInvalidInputError, handleUnexpectedError } = require("./errors");
-const app = express();
-
-app.use(express.json());
-app.use("/api", express.static("public"));
 
 app.get("/api/topics", getTopics);
 app.get("/api/articles", getArticles);
 app.get("/api/users", getUsers);
 app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles/:article_id/comments", getCommentsForArticle);
-
 app.post("/api/articles/:article_id/comments", addCommentToArticle)
 app.patch('/api/articles/:article_id', patchArticleVotes)
 app.delete("/api/comments/:comment_id", deleteCommentById)
